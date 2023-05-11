13a14cd45b6c866e9fedf5030aed81c9b31cfa26
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-05-11 20:41:40 +0200

    Create base template

--- rentservice/admin.py
+++ rentservice/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Worker, Service, OrderedService
 
 # Register your models here.
+admin.site.register(Worker)
+admin.site.register(Service)
+admin.site.register(OrderedService)
