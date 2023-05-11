63dc318607e6a9c58d7d46e679c574167817e2d3
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-05-11 18:51:12 +0200

    Initial commit

---
+++ help4you/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for help4you project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'help4you.settings')
+
+application = get_wsgi_application()
