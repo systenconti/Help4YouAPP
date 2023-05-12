a247179ed73141a99b2260a8ff714e3b41632f2a
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-05-12 23:22:02 +0200

    Fill up Service dropdown list with data in home.html

--- rentservice/urls.py
+++ rentservice/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
+    path(
+        "get_services/<int:profession_id>/",
+        views.get_services_view,
+        name="get_services",
+    ),
 ]
--- rentservice/views.py
+++ rentservice/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Profession, Worker, OrderedService, Service
+from django.http import JsonResponse
 
 
 def home_view(request):
-    return render(request, "home.html")
+    professions = Profession.objects.all()
+    context = {"professions": professions}
+    return render(request, "home.html", context)
+
+
+def get_services_view(request, profession_id):
+    services = Service.objects.filter(profession_id=profession_id).order_by("service")
+    services_list = [
+        {"id": service.id, "service": service.service} for service in services
+    ]
+    data = {"services": services_list}
+    return JsonResponse(data)
